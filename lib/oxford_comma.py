c3b61bf157c811c22a0e38fd5f8a022f88ab5640
Adolf <adolfnyerere@gmail.com>
2023-11-29 11:07:56 +0300

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    # item = []
+    return ''.join(items)
+
+def oxford_comma(items):
+    return ' and '.join(items)
+
+def oxford_comma(items):
+    # if len(items) == 0:
+    #     return ""
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        formatted_items = ", ".join(items[:-1])
+        return f"{formatted_items}, and {items[-1]}"
